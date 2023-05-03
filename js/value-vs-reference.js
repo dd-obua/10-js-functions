34658e64758075a4063efdea19d283f76220dff9
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-03 14:37:30 +0300

    Create checkIn() to see which value would change

--- js/value-vs-reference.js
+++ js/value-vs-reference.js
@@ -2,4 +2,17 @@
 
 // Passing arguments by value vs. by reference
 const flight = 'LH234';
-const passenger = { name: 'Denis', passport: 2378745912 };
+const traveller = { name: 'Denis', passport: 2378745912 };
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = `Mr. ${passenger.name}`;
+
+  if (passenger.passport === 2378745912) console.log('Checked in.');
+  else console.log('Wrong passport.');
+};
+
+checkIn(flight, traveller);
+console.log(flight);
+console.log(traveller);
+console.log();
