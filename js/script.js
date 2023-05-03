1bedd020127ceacada2f4775fc8720aa626ef5a1
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-03 12:19:13 +0300

    Create function that add booking object into the empty array

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 
 // Default parameters
 const bookings = [];
+
+const createBookings = function (flightNum, numPassangers, fare) {
+  const booking = { flightNum, numPassangers, fare };
+  return bookings.push(booking);
+};
