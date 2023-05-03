7a2319ba44ec199189349a1f12969f10ccf3e40c
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-03 20:15:42 +0300

    Rename function

--- js/higher-order-1.js
+++ js/higher-order-1.js
@@ -3,7 +3,7 @@
 'use strict';
 console.log();
 
-const oneWord = str => str.replace(/ /g, '').toLowerCase();
+const toOneWord = str => str.replace(/ /g, '').toLowerCase();
 
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
@@ -21,9 +21,7 @@ const text = 'JavaScript is the best!';
 transformText(text, upperFirstWord);
 console.log();
 
-transformText(text, oneWord);
+transformText(text, toOneWord);
 console.log();
 
 const high5 = () => console.log('👋');
-
-document.body.addEventListener('click', high5);
