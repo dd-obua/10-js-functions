2ec0748a27e05f3613f6081bb0218921d7461f98
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-03 19:14:25 +0300

    Convert the first word of a stirng input to uppercase

--- js/higher-order-1.js
+++ js/higher-order-1.js
@@ -8,3 +8,12 @@ const oneWord = str => str.replace(/ /g, '').toLowerCase();
 let motto = 'For Community Transformation';
 console.log(oneWord(motto));
 console.log();
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+let faithWord = 'God is able';
+console.log(upperFirstWord(faithWord));
+console.log();
