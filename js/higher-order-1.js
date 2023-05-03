147424e7971bfd097754e93bd4c9494b827557d1
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-03 19:07:27 +0300

    Call oneWord() with string input

--- js/higher-order-1.js
+++ js/higher-order-1.js
@@ -1,5 +1,10 @@
 // Functions that accepts other functions as inputs
 
 'use strict';
+console.log();
 
 const oneWord = str => str.replace(/ /g, '').toLowerCase();
+
+let motto = 'For Community Transformation';
+console.log(oneWord(motto));
+console.log();
