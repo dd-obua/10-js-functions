ef84b438279f8be50594159da43c5096db124b3f
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-03 19:15:35 +0300

    Remove function calls

--- js/higher-order-1.js
+++ js/higher-order-1.js
@@ -5,15 +5,7 @@ console.log();
 
 const oneWord = str => str.replace(/ /g, '').toLowerCase();
 
-let motto = 'For Community Transformation';
-console.log(oneWord(motto));
-console.log();
-
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
 };
-
-let faithWord = 'God is able';
-console.log(upperFirstWord(faithWord));
-console.log();
