bc7dc06bc47764210f77a029db042f046462ea10
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-04 09:29:44 +0300

    Create eagleAir object with its method

--- js/call-apply.js
+++ js/call-apply.js
@@ -1,2 +1,13 @@
 // Implementing call and apply methods
 'use strict';
+
+const eagleAir = {
+  airline: 'Eagle Air',
+  iataCode: 'EA',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a flight on ${this.airline}, flight ${this.iataCode}${flightNum}.`
+    );
+  },
+};
