3252b1f2839e1e3f1db9900bdd481649b1d86120
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-05 10:40:59 +0300

    Prompt for input and strore it in a variable

--- challenge/fns.js
+++ challenge/fns.js
@@ -5,4 +5,17 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const input = prompt(`
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+    `);
+  },
 };
+
+poll.registerNewAnswer();
