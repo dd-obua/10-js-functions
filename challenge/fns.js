1d32658484dded07b7f5a65118d698c870735506
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-05 17:05:10 +0300

    Prompt using the options property

--- challenge/fns.js
+++ challenge/fns.js
@@ -7,14 +7,9 @@ const poll = {
   answers: new Array(4).fill(0),
 
   registerNewAnswer() {
-    const input = prompt(`
-        What is your favourite programming language?
-        0: JavaScript
-        1: Python
-        2: Rust
-        3: C++
-        (Write option number)
-    `);
+    const input = prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    );
 
     const inputValue = Number(input);
 
